96763a14ea311772ae4c4eab1bbfa72d534dcab6
XiaoBaiqaz <124913418+XiaoBaiqaz@users.noreply.github.com>
2023-05-29 09:52:50 +0800

    Delete src directory

--- src/main/java/com/example/mybatissb/service/AddRootService.java
+++
@@ -1,9 +0,0 @@
-package com.example.mybatissb.service;
-
-/**
- * 作者：17596
- * 时间：2023/5/2818:21202305
- */
-public interface AddRootService {
-    public int addcg(String id,String mean,int rate);
-}
--- src/main/java/com/example/mybatissb/service/CountEasy.java
+++
@@ -1,9 +0,0 @@
-package com.example.mybatissb.service;
-
-/**
- * 作者：17596
- * 时间：2023/5/2422:07202305
- */
-public interface CountEasy {
-    public int easycount();
-}
